04eaa40243800180342dad8805bead6f59ff1668
Yogeshbairwa <ybairwa786@gmail.com>
2024-01-26 17:11:26 +0530

    .....

--- basics.js
+++ basics.js
@@ -16,9 +16,33 @@ const weekdays = [
 // console.log(weekdays[date.getDay()]);
 console.log("Today is:", weekdays[date.getDay()]);
 
-const hours = date.getHours().toString().padStart(2, 0);
+const hours = date.getHours();
+const hours12 = (hours > 12 ? hours - 12 : hours).toString().padStart(2, 0);
 const minutes = date.getMinutes().toString().padStart(2, 0);
 const seconds = date.getSeconds().toString().padStart(2, 0);
 
-const time = `${hours}${hours > 12 ? "PM" : "AM"}:${minutes}:${seconds}`;
+const time = `${hours12} ${hours > 12 ? "PM" : "AM"}:${minutes}:${seconds}`;
 console.log("Current Time is :", time);
+
+//2. Write a JavaScript program to print the current window contents.
+// window.print();
+const btn = document.getElementById("print");
+btn.addEventListener("click", () => window.print());
+
+//3.Write a JavaScript program to get the current date.
+//Expected Output :
+//mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+const yyyy = date.getFullYear().toString().padStart(2, 0);
+// console.log(yy);
+const mm = date.getMonth().toString().padStart(2, 0);
+// console.log(mm);
+const dd = date.getDay().toString().padStart(2, 0);
+// console.log(dd);
+
+console.log(`${mm}-${dd}-${yyyy}`);
+console.log(`${mm}/${dd}/${yyyy}`);
+console.log(`${dd}-${mm}-${yyyy}`);
+console.log(`${dd}/${mm}/${yyyy}`);
